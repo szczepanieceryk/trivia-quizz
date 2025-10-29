2530d12e89df262716d17989fa7614a13ab33280
eryk <e.szczepaniec@lightpixel.io>
2025-10-29 17:25:42 +0100

    fix(ui): add hover state with transition on close popup icon

--- src/layouts/TriviaQuizz/DifficultySelect.tsx
+++ src/layouts/TriviaQuizz/DifficultySelect.tsx
@@ -7,10 +7,10 @@ const DifficultySelect: React.FC<DifficultySelectProps> = ({ isOpen, setIsOpen }
   const difficultyLevels = ['easy', 'medium', 'hard'];
 
   return (
-    <div className="py-8 px-4 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-2 rounded-lg w-[300px] mx-auto border-gray-200 bg-gray-900">
+    <div className="py-10 px-4 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-2 rounded-lg w-[300px] mx-auto border-gray-200 bg-gray-900">
       <div className="relative">
         <div
-          className="absolute -top-6 -right-2 w-8 h-8 border-2 border-gray-200 rounded-2xl cursor-pointer"
+          className="absolute -top-6 -right-2 w-8 h-8 bg-transparent text-white border-2 border-gray-200 rounded-2xl cursor-pointer hover:bg-gray-300 hover:text-black transition duration-500 ease-in-out"
           onClick={() => setIsOpen(!isOpen)}
         >
           x
